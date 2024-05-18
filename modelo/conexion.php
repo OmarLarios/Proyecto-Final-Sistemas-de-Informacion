b0ac1250a57dbd80a3a0bede783f9bb6bb62a76a
OmarLarios <omarlarios02@aragon.unam.mx>
2024-05-17 23:32:08 -0600

    Correccion

--- modelo/conexion.php
+++ modelo/conexion.php
@@ -1,4 +1,4 @@
 <?php
-$conexion=new mysqli("localhost", "root", "Larios25", "proyectofinal", "3306");//Alumnos
+$conexion=new mysqli("localhost", "root", "Larios25", "ProyectoFinalSI", "3306");//Alumnos
 $conexion->set_charset("utf8");
 ?>
